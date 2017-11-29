9320a077c98d35a162db7a1a6eb5982de9363da5
karthikkennedy <34051482+karthikkennedy@users.noreply.github.com>
2017-11-29 11:19:08 +0530

    python 3rd day activity updated

--- function.py
+++ function.py
@@ -125,3 +125,109 @@ def twoarg(name,age):
     print('hello', name, ',' ,age)
 
 twoarg('karthik',30)
+
+#function with default argument
+def test(name, msg='hardcoded value'):
+    print('hello',name,',', msg)
+
+test('karthik')
+test('kennedy','override the default value')
+
+#passing string as argument
+def stringsample(s):
+    print(s)
+    return
+stringsample("welcome")
+stringsample("string as argument")
+
+#pass by reference , List arg passsing to fun
+def listsample(l):
+    print('inside the fun..',l)
+    return
+
+l=[10,20,30]
+listsample(l)
+print('outside fun..',l)
+
+#pass by reference , List arg passsing to fun
+def listsample(l):
+    print('inside the fun..',l)
+    return
+
+#l=[10,20,30]
+listsample([10,20,30,40])
+print('outside fun..',l) #this time printing above value
+
+#pass by reference , List arg passsing to fun
+def listsample(l):
+    l=[44,55,66]
+    print('inside the fun..',l)
+    return
+
+l=[11,22,33]
+listsample(l)
+print('outside fun..',l)
+
+#argument required
+def stringsample(s):
+    print(s)
+    return
+#stringsample() #arg req
+
+#keyword arg
+def stringsample(s):
+    print(s)
+    return;
+
+stringsample(s="karthik kennedy")
+
+#keyword arg
+def stringsample(name,age):
+    print('Name: ', name,',','Age: ',age)
+    return;
+
+stringsample(name="karthik kennedy",age=31)
+
+#default arg
+def printval(name,age=30):
+    print('Name: ', name, ',', 'Age: ', age)
+    return;
+
+printval(name="karthik", age=32)
+printval(name="sugi")
+
+#arbitary argument
+def show(*name):
+    for names in name:
+        print('hello ',name)
+
+show('karthik','kennedy','sugi','ganesh')
+
+#anonymous fun
+sum=lambda arg1,arg2:arg1+arg2
+
+print('sum of value ',sum(10,20))
+
+#global vs local variable scope
+total=0; #global variable
+def sum(a1,a2):
+    total=a1+a2;
+    print('inside local',total)
+
+sum(20,20)
+print('outside ',total)
+
+#recursive fun
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return (n*fact(n-1))
+
+num=5
+print('factorial of ',num, 'is', fact(num))
+
+
+
+
+
